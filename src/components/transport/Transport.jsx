4b72dfd6c3dd2d18d7921cc9891da04484082c8c
thedank_night <56950335+thedank_night@users.noreply.github.com>
2020-08-24 00:44:27 -0700

    Add logic to hide unneeded buttons in Transport

--- src/components/transport/Transport.jsx
+++ src/components/transport/Transport.jsx
@@ -5,12 +5,11 @@ export default function Transport(props) {
 
   const pauseButton = () => <button onClick={props.onPause}>Pause</button>;
 
-  const resetButton = () => <button onClick={props.onReset}>Reset</button>
+  const resetButton = () => props.status !== 'clean' ? <button onClick={props.onReset}>Reset</button> : null;
 
   return (
     <div>
-      {startButton()}
-      {pauseButton()}
+      {props.status !== 'active' ? startButton() : pauseButton()}
       {resetButton()}
     </div>
   )
