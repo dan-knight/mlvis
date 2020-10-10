bd120a5355162bcedd069e9bccd5e137414e1600
Daniel <56950335+thedank_night@users.noreply.github.com>
2020-10-10 14:49:31 -0700

    Create basic correlation functions
    - Functions use basic arrays (not data-forge).
    
    - Created utility functions for finding sum, mean, error, and covariance.

---
+++ src/logic/stat.js
@@ -0,0 +1,50 @@
+function relationshipExists(xValues, yValues) {
+  const correlation = getCorrelation(xValues, yValues);
+  const threshold = 2 / Math.sqrt(xValues.length);
+
+  return (Math.abs(correlation) >= threshold);
+};
+
+function getCorrelation(xValues, yValues) {
+  const xErrors = getErrors(xValues);
+  const xStdDev = getStdDev(xErrors);
+
+  const yErrors = getErrors(yValues);
+  const yStdDev = getStdDev(yErrors);
+  return getCovariance(xErrors, yErrors) / (xStdDev * yStdDev);
+};
+
+function getCovariance(xErrors, yErrors) {
+  const totalVariance = xErrors.reduce((total, current, i) => (
+    total + (current * yErrors[i])
+  ), 0);
+
+  return totalVariance / (xValues.length - 1);
+};
+
+function getStdDev(errors) {
+  const sumOfSquares = errors.reduce((total, current) => total + (current ** 2), 0);
+  return Math.sqrt(sumOfSquares / (errors.length - 1));
+};
+
+function getErrors(values, mean=null) {
+  if (!mean) {
+    mean = getMean(values);
+  };
+
+  return values.map(v => v - mean);
+};
+
+function getMean(values) {
+  return getSum(values) / values.length;
+};
+
+function getSum(values) {
+  return values.reduce((total, current) => total + current, 0);
+};
+
+const xValues = [12, 30, 15, 24, 14, 18, 28, 26, 19, 27];
+const yValues = [20, 60, 27, 50, 21, 30, 61, 54, 32, 57];
+
+
+console.log(relationshipExists(xValues, yValues));
